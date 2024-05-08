9b3f2ddb555c0d36c2dea696db2b9cc5b8c45446
Bruno Martins <jbrunno17@gmail.com>
2024-05-08 13:14:43 -0300

    Adiciona métodos usando a porpriedade methods

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 const app = Vue.createApp({
-    data(){
+    data() {
         return {
             firstName: 'Bruno',
             lastName: 'Martins',
             email: "BM@gmail.com",
             city: "Patos",
-            picture:  'https://randomuser.me/api/portraits/men/57.jpg'
+            picture: 'https://randomuser.me/api/portraits/men/57.jpg'
+        }
+    },
+
+    methods: {
+        changeData(){
+            this.firstName = 'Caio';
+            this.lastName = 'Almeida';
+            this.email = 'CA@gmail.com';
+            this.city = 'João Pessoa';
+            this.picture = 'https://randomuser.me/api/portraits/men/60.jpg';
         }
     }
 })
