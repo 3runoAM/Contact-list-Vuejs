0d6bd84a8c2ef5e6aaa8d0ecc232efde157596ee
Bruno Martins <jbrunno17@gmail.com>
2024-05-08 15:08:16 -0300

    Imnplementa busca em tempo real usando propriedades computadas

--- main.js
+++ main.js
@@ -1,7 +1,49 @@
 const app = Vue.createApp({
     data() {
         return {
-            contactList: []
+            searchText: '',
+            contactList: [
+                {
+                    firstName: 'Bruno',
+                    lastName: 'Martins',
+                    email: "BM@gmail.com",
+                    city: "Patos",
+                    picture: 'https://randomuser.me/api/portraits/men/57.jpg'
+                },
+                {
+                    firstName: 'Caio',
+                    lastName: 'Almeida',
+                    email: "CA@gmail.com",
+                    city: "Patos",
+                    picture: 'https://randomuser.me/api/portraits/men/58.jpg'
+                },
+                {
+                    firstName: 'Roberto',
+                    lastName: 'Soares',
+                    email: "RS@gmail.com",
+                    city: "Cajazeiras",
+                    picture: 'https://randomuser.me/api/portraits/men/59.jpg'
+                },
+                {
+                    firstName: 'Gernan',
+                    lastName: 'Soares',
+                    email: "GS@gmail.com",
+                    city: "Cajazeiras",
+                    picture: 'https://randomuser.me/api/portraits/men/60.jpg'
+                }
+            ]
+        }
+    },
+
+    computed: {
+        listResult() {
+            if(this.searchText) {
+                return this.contactList.filter(contact => {
+                    return contact.firstName.toLowerCase().includes(this.searchText.toLowerCase());
+                })
+            }else {
+                return this.contactList
+            }
         }
     },
 
@@ -15,7 +57,7 @@ const app = Vue.createApp({
                 contact.lastName = info.name.last
                 contact.email = info.email
                 contact.city = info.location.city
-                contact.picture = info.picture.medium
+                contact.picture = info.picture.large
                 this.contactList.push(contact)
             });
             return contactList
