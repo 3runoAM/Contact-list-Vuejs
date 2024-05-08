5ff88c4a545faee98b6a3c4c045f55a206d25d39
Bruno Martins <jbrunno17@gmail.com>
2024-05-08 14:20:52 -0300

    Lê dados da API Random User, cria objetos e os exibe na tela

--- main.js
+++ main.js
@@ -1,21 +1,24 @@
 const app = Vue.createApp({
     data() {
         return {
-            firstName: 'Bruno',
-            lastName: 'Martins',
-            email: "BM@gmail.com",
-            city: "Patos",
-            picture: 'https://randomuser.me/api/portraits/men/57.jpg'
+            contactList: []
         }
     },
 
     methods: {
-        changeData(){
-            this.firstName = 'Caio';
-            this.lastName = 'Almeida';
-            this.email = 'CA@gmail.com';
-            this.city = 'João Pessoa';
-            this.picture = 'https://randomuser.me/api/portraits/men/60.jpg';
+        async getData(){
+            let response = await fetch("https://randomuser.me/api/?results=15");
+            let data = await response.json()
+            data.results.forEach(info => {
+                let contact = {};
+                contact.firstName = info.name.first
+                contact.lastName = info.name.last
+                contact.email = info.email
+                contact.city = info.location.city
+                contact.picture = info.picture.medium
+                this.contactList.push(contact)
+            });
+            return contactList
         }
     }
 })
